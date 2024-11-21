7814d7c9f8befbb8ebf78cb4a7a2c11271669848
bhavya raval <bhavyabenraval22@gnu.ac.in>
2024-11-21 20:24:21 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/example/prac1/p4.kt
@@ -0,0 +1,12 @@
+package com.example.prac1
+
+fun main() {
+    println("Enter the number")
+    var a:Int=readLine()?.toInt()!!
+//     a=readLine()!!.toInt()
+    println(if(a%2==0){
+        "$a even number"
+    }else{
+        "$a odd number"
+    })
+}
